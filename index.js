959e57f86d6bdd41effecef57f1aab663ffe8a5b
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-05 18:06:58 +0000

    intégration feedbacks

--- index.js
+++ index.js
@@ -54,16 +54,35 @@ document.getElementById("done3").onclick = function Done() {
     document.getElementById("tache3").classList.remove('bg-warning')
     document.getElementById("tache3").classList.remove('bg-danger')
     document.getElementById("tache3").classList.add('bg-success')
+
 }
+
 //**************créationd de tâches********** */
 function Taches() {
     let tache0 = document.querySelector('#tache0').value
-    document.querySelector('#list').innerHTML +=
+    let i = document.getElementById("form2").length
+    document.querySelector('#form2').innerHTML +=
         `<div class="d-flex justify-content-around">
-        <input type="text" value="${tache0}" class="form-control bg-danger" id="tache2">
-        <input type="button" class="btn btn-danger" onclick="Todo()" value="to do" id="todo2">
-        <input type="button" class="btn btn-warning" onclick="Doing()" value="doing" id="doing2">
-        <input type="button" class="btn btn-success" onclick="Done()" value="done" id="done2">
+        <span class="w-100 bg-danger rounded-1 align-self-center py-2" id="tache${i}">${tache0}</span>
+        <input type="button" class="btn btn-danger" onclick="Todo()" value="to do" id="todo${i}">
+        <input type="button" class="btn btn-warning" onclick="Doing()" value="doing" id="doing${i}">
+        <input type="button" class="btn btn-success" onclick="Done()" value="done" id="done${i}">
     </div>`
+    document.getElementById(`done${i}`).onclick = function Done() {
+        document.getElementById(`tache${i}`).classList.remove('bg-warning')
+        document.getElementById(`tache${i}`).classList.remove('bg-danger')
+        document.getElementById(`tache${i}`).classList.add('bg-success')
+
+    }
+    document.getElementById(`doing${i}`).onclick = function Doing() {
+        document.getElementById(`tache${i}`).classList.remove('bg-danger')
+        document.getElementById(`tache${i}`).classList.remove('bg-success')
+        document.getElementById(`tache${i}`).classList.add('bg-warning')
+    }
+    document.getElementById(`todo${i}`).onclick = function Todo() {
+        document.getElementById(`tache${i}`).classList.remove("bg-warning")
+        document.getElementById(`tache${i}`).classList.remove("bg-success")
+        document.getElementById(`tache${i}`).classList.add("bg-danger")
+
+    }
 }
- 
