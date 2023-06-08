3e141c8695b46005dd3689942ec46523161647c2
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-08 10:17:49 +0000

    intégrations feedbacks

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-
+//selection de tout les boutons /
 let done = document.querySelectorAll(".done");
 let doing = document.querySelectorAll(".doing");
 let todo = document.querySelectorAll(".todo");
+let trash = document.querySelectorAll(".btn")
+//*********bouton dtodo*****/
 
-        function Done() {
-            event.target.parentNode.style.background = "#198754"
-        }
+function Todo() {
 
-        function Doing (){
-            event.target.parentNode.style.background = "#ffc107"
-        }
+    event.target.parentNode.style.background = "#dc3545"
 
-        function Todo() {
+}
+//*********bouton doing*****/
 
-            event.target.parentNode.style.background = "#dc3545"
-                
-        }
+function Doing() {
+    event.target.parentNode.style.background = "#ffc107"
+}
 
-//**************créationd de tâches********** */
+//*********boutton done*****/
+
+function Done() {
+    event.target.parentNode.style.background = "#198754"
+}
+
+
+//**************création de tâches********** */
 function Taches() {
-    
+
     let tache0 = document.querySelector('#tache0').value
 
     document.querySelector('#form2').innerHTML +=
         `<div class="d-flex justify-content-around align-items-center my-2 rounded border border-danger">
         ${tache0}
-        <input type="button" class="btn btn-danger todo" onclick="Todo()" value="to do" id="todo1">
-        <input type="button" class="btn btn-warning doing"  onclick="Doing()"value="doing" id="doing1">
-        <input type="button" class="btn btn-success done" onclick="Done()" value="done" id="done1">
+        <input type="button" class="btn btn-danger todo" onclick="Todo()" value="to do">
+        <input type="button" class="btn btn-warning doing"  onclick="Doing()"value="doing">
+        <input type="button" class="btn btn-success done" onclick="Done()" value="done">
     </div>`
 
 }
