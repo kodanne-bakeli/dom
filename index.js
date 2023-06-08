29afdd5815d7965f91831bcc95cd02a215d708af
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-08 23:13:27 +0000

    intégration feedbacks

--- index.js
+++ index.js
@@ -22,17 +22,16 @@ function Done() {
     event.target.parentNode.style.background = "#198754"
 }
 
-
 //**************création de tâches********** */
 function Taches() {
 
     let tache0 = document.querySelector('#tache0').value
 
     document.querySelector('#form2').innerHTML +=
-        `<div class="d-flex justify-content-around align-items-center my-2 rounded border border-danger">
+        `<div class="d-flex justify-content-around align-items-center w-75 mx-auto my-2 p-2 rounded border border-danger">
         ${tache0}
         <input type="button" class="btn btn-danger todo" onclick="Todo()" value="to do">
-        <input type="button" class="btn btn-warning doing"  onclick="Doing()"value="doing">
+        <input type="button" class="btn btn-warning doing" onclick="Doing()" value="doing">
         <input type="button" class="btn btn-success done" onclick="Done()" value="done">
     </div>`
 
